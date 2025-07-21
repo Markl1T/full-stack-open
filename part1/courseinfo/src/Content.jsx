b665d069b588fd41930906206a7371e621d30aa5
Markli Tati <79922926+Markl1T@users.noreply.github.com>
2025-07-21 22:31:42 +0200

    complete Course Information step 3

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -3,9 +3,9 @@ import Part from './Part'
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part name={props.name1} exercises={props.exercises1} />
+      <Part name={props.name2} exercises={props.exercises2} />
+      <Part name={props.name3} exercises={props.exercises3} />
     </>
   )
 }
