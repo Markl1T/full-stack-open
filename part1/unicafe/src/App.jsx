367634f7b5bca1317a4a949dbc11ec0ba727a3a9
Markli Tati <79922926+Markl1T@users.noreply.github.com>
2025-07-24 06:08:13 +0200

    complete unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,12 +5,18 @@ const Button = ({onClick, text}) => {
 }
 
 const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const sum = good - bad
+  
   return (
     <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {sum / all}</p>
+      <p>positive {good / all * 100} %</p>
     </>
   )
 }
