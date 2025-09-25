9692f9eb37c4702134a600ef448448b319315f79
Markli Tati <79922926+Markl1T@users.noreply.github.com>
2025-09-25 15:01:18 +0200

    add frontend production build

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
-
 app.use(express.json())
+app.use(express.static('dist'))
 
 const cors = require('cors')
 app.use(cors())
 
 const morgan = require('morgan')
 morgan.token('body', (req) => { return JSON.stringify(req.body) })
-
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
