1932982365ec5dc13b3a9004c75906e0b148689f
Markli Tati <79922926+Markl1T@users.noreply.github.com>
2025-10-15 23:59:10 +0200

    complete part4 ex. 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((likes, blog) => likes + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	if(blogs.length === 0) {
+		return null
+	}
+	return blogs.reduce((mostLiked, current) => {
+		return current.likes > mostLiked.likes ? current : mostLiked
+	})
+}
+
 module.exports = {
   dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
